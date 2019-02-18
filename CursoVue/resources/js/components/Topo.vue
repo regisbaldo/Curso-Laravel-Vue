dc6e5e3ee13884f2e5a5060578c0d7b3a5675e83
Reginaldo Bonaldi <reginaldobonaldi22@gmail.com>
2019-02-18 20:00:20 -0300

    primeiro commit

---
+++ CursoVue/resources/js/components/Topo.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+      <div class="container">
+          <a class="navbar-brand" v-bind:href="url">
+              {{ titulo }}
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" v-bind:aria-label="toogle">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <!-- Left Side Of Navbar -->
+              <ul class="navbar-nav mr-auto">
+                <slot></slot>
+              </ul>
+
+              <!-- Right Side Of Navbar -->
+              <ul class="navbar-nav ml-auto">
+                <slot></slot>
+              </ul>
+          </div>
+      </div>
+  </nav>
+
+</template>
+
+<script>
+    export default {
+      props:['url','titulo','toogle']
+    }
+</script>
